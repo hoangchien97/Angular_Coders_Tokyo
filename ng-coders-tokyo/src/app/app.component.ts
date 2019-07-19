6eeb9f978be4ade55847b80562d8daf0ebe131cc
HoangChien <chienhd@icheck.vn>
2019-07-19 14:14:40 +0700

    component

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // document.querySelector('app-root'); <=> <app-root></app-root>
-  // document.querySelector('.app-root'); <=> <div class="app-root"></divs>
-  templateUrl: './app.component.html',
-  // C1 : đường link
-  // C2 : // `<h1>test</h1>`
+  // document.querySelector('.app-root'); <=> <div class="app-root"></div>
+  template: `
+    <h1>Welcome to Coders.Tokyo</h1>
+    <app-hello></app-hello>
+    <app-hi></app-hi>
+  `,
   styleUrls: ['./app.component.css']
   // tương tự teamplate . CSS
 })
