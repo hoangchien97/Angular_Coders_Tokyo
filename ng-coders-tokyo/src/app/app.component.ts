d69662cf11b86a7e42ec4bedcfccac23ce1ed346
Chien Hoang <hoangducchien97@gmail.com>
2019-07-20 23:38:30 +0700

    bind class

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -2,16 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // document.querySelector('app-root'); <=> <app-root></app-root>
-  // document.querySelector('.app-root'); <=> <div class="app-root"></div>
   template: `
-    <h1>Welcome to Coders.Tokyo</h1>
-    <app-hello></app-hello>
-    <app-hi></app-hi>
+    <h1 [ngStyle] = "styleObj">Welcome to {{ title }}</h1>
+    <h2 [class.with-border]="withBorder">Class binding</h2>
+    <h1 [textContent]="title"></h1>
+    <img src="{{imageUrl}}"/>
+    <img [src]="imageUrl" />
   `,
   styleUrls: ['./app.component.css']
-  // tương tự teamplate . CSS
 })
 export class AppComponent {
-  title = 'Hello World';
+  title = 'Coders Tokyo Update'; // public
+  imageUrl = 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  textColor = 'tomato';
+  backgroundColor = 'yellow';
+  styleObj = {color: this.textColor, background: this.backgroundColor };
+  withBorder = true;
 }
+
+// Data binding
+
+// 1. Property binding
+// 2. Event binding
+
+
+// Access modifier
+// public, private, ...
+
+// <img [src]="imageUrl"/> // Squarebrakets
+
+// [class] [att.id] // document.getElementById(...).attribute
+
+// [style.color] = "textColor" [style.background] = "backgroundColor"
+// <=> [ngStyle] = "{color:textColor,background: backgroundColor}"
+
